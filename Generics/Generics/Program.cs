9037906446a1c9f800a09d87a2a16a962af89f92
CDinuwan <chanukadinuwan35@gmail.com>
2021-03-20 22:02:30 +0530

    Generics

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = new Result<int,bool> { Success = 10, Data = true };
+
+            Console.WriteLine(result.Success);
+            Console.WriteLine(result.Data);
+        }
+    }
+    public class Result<T,U>
+    {
+        public T Success { get; set; }
+        public U Data { get; set; }
+    }
+}
