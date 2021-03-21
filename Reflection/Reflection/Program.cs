2265e3e2aba4a2f7087484488c01a1ec7a8c920d
CDinuwan <chanukadinuwan35@gmail.com>
2021-03-21 10:45:46 +0530

    reflection

--- Reflection/Reflection/Program.cs
+++ Reflection/Reflection/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 
 namespace Reflection
 {
@@ -6,7 +7,49 @@ namespace Reflection
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var assembly = Assembly.GetExecutingAssembly();
+            Console.WriteLine(assembly.FullName);
+
+            var types = assembly.GetTypes();
+            foreach(var type in types)
+            {
+                Console.WriteLine("Type: " + type.Name+ " Base Type"+type.BaseType);
+                var props = type.GetProperties();
+                foreach (var prop in props)
+                {
+                    Console.WriteLine("\t"+prop.Name);
+                }
+
+                var fields = type.GetFields();
+                foreach (var field in fields)
+                {
+                    Console.WriteLine("\t"+field.Name);
+                }
+
+                var methods = type.GetMethods();
+                foreach(var method in methods)
+                {
+                    Console.WriteLine(method.Name);
+                }
+            }
+            var sample = new Sample
+            {
+
+            };
+            var sampleType = typeof(Sample);
+            var myMethod = sampleType.GetMethod("MyMethod");
+            Console.WriteLine(myMethod);
+
+            myMethod.Invoke(sample,null);
         }
+    }
+}public class Sample
+{
+    public string Name { get; set; }
+    public int age;
+
+    public void MyMethod()
+    {
+        Console.WriteLine("This is my method");
     }
 }
