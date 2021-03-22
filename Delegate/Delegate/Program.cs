95e911e1f0cc36e08b259689560be58d2dbdd90d
CDinuwan <chanukadinuwan35@gmail.com>
2021-03-22 18:48:14 +0530

    Delegates

---
+++ Delegate/Delegate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Delegate
+{
+    class Program
+    {
+        delegate void Operation(int number);
+        static void Main(string[] args)
+        {
+            Operation opration = Double;
+            ExecuteOperation(2, opration);
+        }
+        static void Double(int num)
+        {
+            Console.WriteLine("{0}*2={1}",num,num*2);
+        }
+        static void ExecuteOperation(int num,Operation op)
+        {
+            op(num);
+        }
+    }
+}
