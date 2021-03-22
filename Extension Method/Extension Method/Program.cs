ed460a08a6bdda6961233091cc09b3c8cc5f2df0
CDinuwan <chanukadinuwan35@gmail.com>
2021-03-22 21:40:07 +0530

    Extension Method

---
+++ Extension Method/Extension Method/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Extension_Method
+{
+    public class Person
+    {
+        public int Age { get; set; }
+        public string  Name { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p = new Person { Name = "Chanuka", Age = 24 };
+            p.SayHello();
+        }
+    }
+    public static class Extensions
+    {
+        public static void SayHello(this Person person)
+        {
+            Console.WriteLine(person.Name);
+        }
+    }
+}
