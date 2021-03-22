e6b349adc1e4a2952a05701a381ec5aa972d0199
CDinuwan <chanukadinuwan35@gmail.com>
2021-03-22 19:14:57 +0530

    Annonymous methods

---
+++ Annonymous Method/Annonymous Method/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Annonymous_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<int> op = num=>{ Console.WriteLine("{0} * 2= {1}",num,num*2);};
+
+            Func<int, int> Double = x => { return x*2; };
+
+            Console.WriteLine(Double(2));
+
+            Console.WriteLine(op);
+        }
+    }
+}
