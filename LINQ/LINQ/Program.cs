5790188ff37a6994396361a68fe67e536b7b0251
CDinuwan <chanukadinuwan35@gmail.com>
2021-03-22 22:09:22 +0530

    LINQ

---
+++ LINQ/LINQ/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sample = "I enjoy writing uber-software";
+            var result = from c in sample
+                         where c == 's' || c == 'e' || c == 'f'
+                         orderby c
+                         select c;
+            foreach(var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
