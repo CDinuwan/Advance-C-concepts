bbfabd55de8336cb5e679f08e23362118c441695
CDinuwan <chanukadinuwan35@gmail.com>
2021-03-23 00:35:15 +0530

    Anonymous type

---
+++ Anonymous Types/Anonymous Types/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Anonymous_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new List<Car>
+            {
+                new Car{Wheels=4,Colours="Red"},
+                new Car{Wheels=6,Colours="Blue"}
+            };
+            var result = from c in car
+                         where c.Colours == "Blue"
+                         select new { Wheels = c.Wheels, Colours = c.Colours };
+
+            foreach (var c in result)
+            {
+                Console.WriteLine(c.Colours+" "+c.Wheels);
+            }
+        }
+    }
+    public class Car
+    {
+        public int Wheels { get; set; }
+        public string Colours { get; set; }
+    }
+}
