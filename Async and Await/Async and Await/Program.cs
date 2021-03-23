c976cc663b28246f0d800265757e226365e27f6b
CDinuwan <chanukadinuwan35@gmail.com>
2021-03-23 12:47:13 +0530

    async await

---
+++ Async and Await/Async and Await/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace Async_and_Await
+{
+    class Program
+    {
+        public static string BigLongImportantMethod(string name)
+        {
+            Thread.Sleep(2000);
+            return "Hello, " + name;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BigLongImportantMethod("Chanuka"));
+        }
+    }
+}
